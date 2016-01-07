74c6f60a67df964c751ae2084d732538e03d4919
pemrouz <p3pedram@hotmail.com>
2016-01-07 16:51:38 +0000

    update dependencies and simplifying by using consts

--- dist/index.js
+++ dist/index.js
@@ -13,13 +13,21 @@ var _client = require('utilise/client');
 
 var _client2 = _interopRequireDefault(_client);
 
+var _values = require('utilise/values');
+
+var _values2 = _interopRequireDefault(_values);
+
+var _proxy = require('utilise/proxy');
+
+var _proxy2 = _interopRequireDefault(_proxy);
+
 var _attr = require('utilise/attr');
 
 var _attr2 = _interopRequireDefault(_attr);
 
-var _wrap = require('utilise/wrap');
+var _from = require('utilise/from');
 
-var _wrap2 = _interopRequireDefault(_wrap);
+var _from2 = _interopRequireDefault(_from);
 
 var _all = require('utilise/all');
 
@@ -29,9 +37,29 @@ var _raw = require('utilise/raw');
 
 var _raw2 = _interopRequireDefault(_raw);
 
+var _str = require('utilise/str');
+
+var _str2 = _interopRequireDefault(_str);
+
 var _key = require('utilise/key');
 
 var _key2 = _interopRequireDefault(_key);
+
+var _not = require('utilise/not');
+
+var _not2 = _interopRequireDefault(_not);
+
+var _by = require('utilise/by');
+
+var _by2 = _interopRequireDefault(_by);
+
+var _is = require('utilise/is');
+
+var _is2 = _interopRequireDefault(_is);
+
+var _el = require('utilise/el');
+
+var _el2 = _interopRequireDefault(_el);
 
 /* istanbul ignore next */
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
@@ -45,17 +73,17 @@ function precss(ripple) {
 
   ripple.render = render(ripple)(ripple.render);
 
-  values(ripple.types).filter(by('header', 'text/css')).map(function (type) {
-    return type.render = proxy(type.render, css(ripple));
+  (0, _values2.default)(ripple.types).filter((0, _by2.default)('header', 'text/css')).map(function (type) {
+    return type.render = (0, _proxy2.default)(type.render, css(ripple));
   });
 
   return ripple;
 }
 
-function render(ripple) {
+var render = function render(ripple) {
   return function (next) {
     return function (host) {
-      var css = str((0, _attr2.default)(host, 'css')).split(' ').filter(Boolean),
+      var css = (0, _str2.default)((0, _attr2.default)(host, 'css')).split(' ').filter(Boolean),
           root = host.shadowRoot || host,
           head = document.head,
           shadow = head.createShadowRoot && host.shadowRoot,
@@ -65,17 +93,17 @@ function render(ripple) {
       if (!css.length) return next(host);
 
       // this host has a css dep, but it is not loaded yet - stop rendering this host
-      if (css.some(not(is.in(ripple.resources)))) return;
+      if (css.some((0, _not2.default)(_is2.default.in(ripple.resources)))) return;
 
       // retrieve styles
-      styles = css.map(from(ripple.resources)).map((0, _key2.default)('body')).map(scope(host, shadow, css));
+      styles = css.map((0, _from2.default)(ripple.resources)).map((0, _key2.default)('body')).map(scope(host, shadow, css));
 
       // reuse or create style tag
       css.map(function (d) {
-        return (0, _raw2.default)('style[resource="' + d + '"]', shadow ? root : head) || el('style[resource=' + d + ']');
+        return (0, _raw2.default)('style[resource="' + d + '"]', shadow ? root : head) || (0, _el2.default)('style[resource=' + d + ']');
       }).map((0, _key2.default)('innerHTML', function (d, i) {
         return styles[i];
-      })).filter(not(by('parentNode'))).map(function (d) {
+      })).filter((0, _not2.default)((0, _by2.default)('parentNode'))).map(function (d) {
         return shadow ? root.insertBefore(d, root.firstChild) : head.appendChild(d);
       });
 
@@ -83,9 +111,9 @@ function render(ripple) {
       return next(host);
     };
   };
-}
+};
 
-function scope(el, shadow, names) {
+var scope = function scope(el, shadow, names) {
   return shadow ? _identity2.default : function (styles, i) {
     var prefix = '[css~="' + names[i] + '"]',
         escaped = '\\[css~="' + names[i] + '"\\]';
@@ -105,13 +133,13 @@ function scope(el, shadow, names) {
       return $2 + " " + prefix;
     }); // :host(...) -> tag...
   };
-}
+};
 
-function css(ripple) {
+var css = function css(ripple) {
   return function (res) {
     return (0, _all2.default)('[css~="' + res.name + '"]:not([inert])').map(ripple.draw);
   };
-}
+};
 
 var log = require('utilise/log')('[ri/precss]'),
     err = require('utilise/err')('[ri/precss]');
--- src/index.js
+++ src/index.js
@@ -14,68 +14,66 @@ export default function precss(ripple){
   return ripple
 }
 
-function render(ripple){
-  return next => {
-    return host => {
-      var css = str(attr(host, 'css')).split(' ').filter(Boolean)
-        , root = host.shadowRoot || host
-        , head = document.head
-        , shadow = head.createShadowRoot && host.shadowRoot
-        , styles
+const render = ripple => next => host => {
+  var css = str(attr(host, 'css')).split(' ').filter(Boolean)
+    , root = host.shadowRoot || host
+    , head = document.head
+    , shadow = head.createShadowRoot && host.shadowRoot
+    , styles
 
-      // this host does not have a css dep, continue with rest of rendering pipeline
-      if (!css.length) return next(host)
-      
-      // this host has a css dep, but it is not loaded yet - stop rendering this host
-      if (css.some(not(is.in(ripple.resources)))) return;
+  // this host does not have a css dep, continue with rest of rendering pipeline
+  if (!css.length) return next(host)
+  
+  // this host has a css dep, but it is not loaded yet - stop rendering this host
+  if (css.some(not(is.in(ripple.resources)))) return;
 
-      // retrieve styles
-      styles = css
-        .map(from(ripple.resources))
-        .map(key('body'))
-        .map(scope(host, shadow, css))
+  // retrieve styles
+  styles = css
+    .map(from(ripple.resources))
+    .map(key('body'))
+    .map(scope(host, shadow, css))
 
-      // reuse or create style tag
-      css
-        .map(d => raw(`style[resource="${d}"]`, shadow ? root : head) || el(`style[resource=${d}]`))
-        .map(key('innerHTML', (d, i) => styles[i]))
-        .filter(not(by('parentNode')))
-        .map(d => shadow ? root.insertBefore(d, root.firstChild) : head.appendChild(d))
+  // reuse or create style tag
+  css
+    .map(d => raw(`style[resource="${d}"]`, shadow ? root : head) || el(`style[resource=${d}]`))
+    .map(key('innerHTML', (d, i) => styles[i]))
+    .filter(not(by('parentNode')))
+    .map(d => shadow ? root.insertBefore(d, root.firstChild) : head.appendChild(d))
 
-      // continue with rest of the rendering pipeline
-      return next(host)
-    }
-  }
+  // continue with rest of the rendering pipeline
+  return next(host)
 }
 
-function scope(el, shadow, names){
-  return shadow ? identity : (styles, i) => {
-    var prefix  = `[css~="${names[i]}"]`
+const scope = (el, shadow, names) => shadow ? identity : (styles, i) => {
+  const prefix  = `[css~="${names[i]}"]`
       , escaped = `\\[css~="${names[i]}"\\]`
 
-    return styles
-      .replace(/^(?!.*:host)([^@%\n]*){/gim, $1 => prefix+' '+$1 )            // ... {      -> tag ... {
-      .replace(/^(?!.*:host)(.*?),\s*$/gim, $1 => prefix+' '+$1)              // ... ,      -> tag ... ,
-      .replace(/:host\((.*?)\)/gi, ($1, $2) => prefix+$2)                     // :host(...) -> tag...
-      .replace(/:host /gi, prefix + " ")                                      // :host      -> tag
-      .replace(/\/deep\/ /gi, '')                                             // /deep/     -> 
-      .replace(/^.*:host-context\((.*)\)/gim, ($1, $2) => $2 + " " + prefix)  // :host(...) -> tag...
-  }
+  return styles
+    .replace(/^(?!.*:host)([^@%\n]*){/gim, $1 => prefix+' '+$1 )            // ... {      -> tag ... {
+    .replace(/^(?!.*:host)(.*?),\s*$/gim, $1 => prefix+' '+$1)              // ... ,      -> tag ... ,
+    .replace(/:host\((.*?)\)/gi, ($1, $2) => prefix+$2)                     // :host(...) -> tag...
+    .replace(/:host /gi, prefix + " ")                                      // :host      -> tag
+    .replace(/\/deep\/ /gi, '')                                             // /deep/     -> 
+    .replace(/^.*:host-context\((.*)\)/gim, ($1, $2) => $2 + " " + prefix)  // :host(...) -> tag...
 }
 
-function css(ripple) {
-  return res => {
-    return all(`[css~="${res.name}"]:not([inert])`)
-      .map(ripple.draw)
-  }
-}
+const css = ripple => res => 
+  all(`[css~="${res.name}"]:not([inert])`)
+    .map(ripple.draw)
 
 import identity from 'utilise/identity'
 import client from 'utilise/client'
+import values from 'utilise/values'
+import proxy from 'utilise/proxy'
 import attr from 'utilise/attr'
-import wrap from 'utilise/wrap'
+import from from 'utilise/from'
 import all from 'utilise/all'
 import raw from 'utilise/raw'
+import str from 'utilise/str'
 import key from 'utilise/key'
-var log = require('utilise/log')('[ri/precss]')
-  , err = require('utilise/err')('[ri/precss]')
+import not from 'utilise/not'
+import by from 'utilise/by'
+import is from 'utilise/is'
+import el from 'utilise/el'
+const log = require('utilise/log')('[ri/precss]')
+    , err = require('utilise/err')('[ri/precss]')
